85f7f8a478d258b0b29723284bb6e9e581eca2c4
ericmartineau <smartytime@gmail.com>
2020-02-18 16:54:18 -0700

    Working on injector to allow factories

--- kiwi_generator/lib/src/injector_generator.dart
+++ kiwi_generator/lib/src/injector_generator.dart
@@ -2,13 +2,11 @@ import 'package:analyzer/dart/constant/value.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:build/build.dart';
-import 'package:code_builder/code_builder.dart';
-
 import 'package:build/src/builder/build_step.dart';
-import 'package:source_gen/source_gen.dart';
+import 'package:code_builder/code_builder.dart';
 import 'package:dart_style/dart_style.dart';
-
 import 'package:kiwi/kiwi.dart';
+import 'package:source_gen/source_gen.dart';
 
 const TypeChecker _registerTypeChecker = const TypeChecker.fromRuntime(Register);
 
